95133f000639c5fea8809f8a5b6739818598f7e1
Vedant-tec <vedantarha@gmail.com>
2024-10-05 20:27:46 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+# Find More Emoji Here https://www.webfx.com/tools/emoji-cheat-sheet/
+st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
+
+# --------------------------------- HEADER SECTION ---------------------------
+st.subheader("Hi, I am Vedant :wave:")
+st.title("A Student From Jaipur")
+st.write("Love Coding")
